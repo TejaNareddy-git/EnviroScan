32d5dad74c0376a261ad43b60956d7bd5ae8b7e1
TejaNareddy-git <tejntkr@gmail.com>
2025-10-07 17:17:04 +0530

    Delete app (3).py
    delete

--- app (3).py
+++
@@ -1,6 +0,0 @@
-
-import streamlit as st
-
-st.set_page_config(page_title="Streamlit + ngrok", page_icon="🚀")
-st.title("🚀 Hello from Streamlit!")
-st.write("This app runs inside Colab and is accessible via ngrok tunnel.")
